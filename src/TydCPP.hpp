5965e891ec87bbdd833ca0a5f5d9adf6d368b76b
Sven65 <Sven65@users.noreply.github.com>
2021-09-03 12:56:55 +0200

    Init project

---
+++ src/TydCPP.hpp
@@ -0,0 +1,21 @@
+#ifndef H_TYD_CPP
+#define H_TYD_CPP
+
+namespace TydCPP {
+	class Arithmetic {
+		public:
+			// Returns a + b
+			static double Add(double a, double b);
+
+			// Returns a - b
+			static double Subtract(double a, double b);
+
+			// Returns a * b
+			static double Multiply(double a, double b);
+
+			// Returns a / b
+			static double Divide(double a, double b);
+	};
+}
+
+#endif
---
+++ test/demo.cpp
@@ -0,0 +1,9 @@
+#include "TydCPP.hpp"
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world" << std::endl;
+
+	return 0;
+}
